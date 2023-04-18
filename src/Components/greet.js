83629689b8ce952df0aeb1a2c74e894ec295f0a1
Roshan Gupta <43931122+rookie2410@users.noreply.github.com>
2023-04-18 22:29:36 +0530

    Destructuring in Function component

--- src/Components/greet.js
+++ src/Components/greet.js
@@ -1,13 +1,39 @@
 import React from 'react'
 
+
+//Destructuring in function parameter
+// const Greet = ({name , hero}) => {
+//     return (
+//       <div>
+//         <h1>
+//           Hello {name} a.k.a {hero}
+//         </h1>
+//       </div>
+//     )
+//   }
+
+
+// Destructuring in function body
 const Greet = props => {
+    const {name,hero}=props
   return (
     <div>
       <h1>
-        Hello {props.name} a.k.a {props.hero}
+        Hello {name} a.k.a {hero}
       </h1>
     </div>
   )
 }
+
+//Without destructuring 
+// const Greet = ({name , hero}) => {
+//     return (
+//       <div>
+//         <h1>
+//           Hello {name} a.k.a {hero}
+//         </h1>
+//       </div>
+//     )
+//   }
 
 export default Greet
