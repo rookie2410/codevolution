8860a17ffe7b56a80dd21f7f153d4025c7d7e96e
Roshan Gupta <43931122+rookie2410@users.noreply.github.com>
2023-04-18 19:42:24 +0530

    func -> class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import welcome from './Components/welcome';
+import counter from './Components/counter';
 
-function App() {
+
+class App extends Component {
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <welcome/>
+      <counter/>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/Components/welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class welcome extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Wecome User</h1>
+      </div>
+    )
+  }
+}
+
+export default welcome
